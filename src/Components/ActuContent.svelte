b0c4df9e1795aba015102e398184463dd0e20f11
Art Goldhammer <art.goldhammer@gmail.com>
2023-07-26 11:32:38 -0400

    changed styles in ActuContent

--- src/Components/ActuContent.svelte
+++ src/Components/ActuContent.svelte
@@ -1,31 +1,58 @@
 <script lang=ts>
-    export let articles
+    interface Articles {
+        id: string
+        title: string
+        summary: string
+        pubdate: string
+    }
+
+    export let articles: Articles[]
 </script>
 
 <div class="content">
+    {#each articles as article}
+         <!-- content here -->
+    
+    <div class="card">
+        <div class="cardhdr">
+            <span class="pubdate">[{article.pubdate}:]</span>
+            <span>{article.title}</span>
+        </div>
+        <div class="cardbody">
+            <p>{article.summary}</p>
+            
+        </div>
+        
+    </div>
+    {/each}
 
-    <ul>
-        {#each articles as article}
-        <li>{article.id}</li>
-        <li>{article.title}</li>
-        <li>{article.summary}</li>
-        <li>{article.pubdate}</li>
-        {/each}
-    </ul>
-
-    <!-- <ul>
-        <li>First</li>
-        <li>Second</li>
-        <li>Third</li>
-    </ul> -->
 </div>
 
 <style>
-    .content {
+    .cardhdr {
+        padding: 2px;
+        padding-left: 8px;
+        margin-top: 10px;
+        border-top: solid 2px black;
+        border-left: solid 2px black;
+        border-right: solid 2px black;
+        border-top-left-radius: 10px;
+        border-top-right-radius: 10px;
         background-color: seashell;
     }
-    li {
-        background-color: lavenderblush;
-        margin: 1em;
+
+    .cardbody {
+        padding-left: 16px;
+        border: solid 2px blue;
+        border-style: groove;
+        border-bottom-left-radius: 10px;
+        border-bottom-right-radius: 10px;
+        background-color: lightblue;
+        /* border-top-style: none; */
+    }
+
+    .pubdate {
+        font-size: smaller;
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
     }
 </style>
