152362a0184e604805b7e5b28acc5e04dc26410f
Art Goldhammer <art.goldhammer@gmail.com>
2023-08-01 20:20:36 -0400

    working with param query

--- netlify/functions/conn.js
+++ netlify/functions/conn.js
@@ -6,17 +6,17 @@ exports.handler = async (event, context) => {
 	// console.log(process.env.MONGO_URI);
 	const client = new MongoClient(process.env.MONGO_URI);
 	console.log('conn: starting mongo client');
-	const time = event.queryStringParameters.time;
-	console.log('conn: time:', time);
+	const timeago = event.queryStringParameters.timeago;
+	console.log('conn: timeago:', timeago);
 	const now = new Date();
-	const then = subHours(now, 3);
+	const then = subHours(now, timeago);
 	console.log('conn: now, then: ', now, then);
 	const db = client.db();
 	const articles = db.collection('articles');
 	const data = await articles
 		.find(
-			{},
-			{ limit: 30, projection: { _id: 0, title: 1, summary: 1, pubdate: 1, pubname: 1, link: 1 } }
+			{ pubdate: { $gte: then, $lte: now } },
+			{ projection: { _id: 0, title: 1, summary: 1, pubdate: 1, pubname: 1, link: 1 } }
 		)
 		.sort({ pubdate: -1 })
 		.toArray();
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,8 +1,7 @@
 export const ssr = false;
 
 export const load = async function ({fetch, params}) {
-	console.log("conn: params: ", params)
-	const response = await fetch('/.netlify/functions/conn?time=3')
+	const response = await fetch('/.netlify/functions/conn?timeago=3')
           .then(response => response.json()
           )
 	// console.dir(response)
