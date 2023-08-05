c2f9d896f465fef9f631949edaa8b0a48a9e55c6
Art Goldhammer <art.goldhammer@gmail.com>
2023-08-05 10:13:32 -0400

    change db.close to client.close

--- netlify/functions/conn.js
+++ netlify/functions/conn.js
@@ -24,7 +24,7 @@ exports.handler = async (event, context) => {
 		)
 		.sort({ pubdate: -1 })
 		.toArray();
-	db.close();
+	client.close();
 	// console.log(data);
 	const reply = {
 		articles: data,
