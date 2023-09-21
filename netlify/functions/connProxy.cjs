23a52c76e6eed0565cc4381d0696aab4525f5b88
Arthur Goldhammer <art.goldhammer@gmail.com>
2023-09-21 10:12:11 -0400

    renamed connProxy to cjs

---
+++ netlify/functions/connProxy.cjs
@@ -0,0 +1,16 @@
+exports.handler = async event => {
+  //   console.log ('event', event);
+  const tf = event.queryStringParameters.timeframe || '0';
+  //   console.log ('tf', tf);
+  const proxy_uri = process.env.PROXY_URI;
+  const uri = `${proxy_uri}/?timeframe=${tf}`;
+  //   console.log ('connProxy uri', uri);
+  const body = await fetch (uri).then (resp => resp.json ());
+  //   console.log (body);
+  //   .then (resp => resp.json ()).then (data => console.log (data));
+  // TODO decoding/encoding json is inefficient; do something with stream passthrough?
+  return {
+    statusCode: 200,
+    body: JSON.stringify (body),
+  };
+};
