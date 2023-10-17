8565339cdad5cb846015b2ec7442e10ab4513291
Arthur Goldhammer <art.goldhammer@gmail.com>
2023-10-16 20:26:51 -0400

    fixed connProxy

--- netlify/functions/connProxy.cjs
+++ netlify/functions/connProxy.cjs
@@ -2,9 +2,15 @@ exports.handler = async event => {
   //   console.log ('event', event);
   const tf = event.queryStringParameters.timeframe || '0';
   const tw = event.queryStringParameters.timewindow || '2';
+  const tx = event.queryStringParameters.txtquery;
   console.log ('tf', tf);
   const proxy_uri = process.env.PROXY_URI;
-  const uri = `${proxy_uri}/?timeframe=${tf}&timewindow=${tw}`;
+  let uri;
+  if (tx) {
+    uri = `${proxy_uri}/?timeframe=${tf}&timewindow=${tw}&txtquery=${tx}`;
+  } else {
+    uri = `${proxy_uri}/?timeframe=${tf}&timewindow=${tw}`;
+  }
   console.log ('connProxy uri', uri);
   const body = await fetch (uri).then (resp => resp.json ());
   // TODO decoding/encoding json is inefficient; do something with stream passthrough?
