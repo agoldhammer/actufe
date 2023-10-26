aaba1ff71da72795ad381ddfe051a2426c0ff33e
Arthur Goldhammer <art.goldhammer@gmail.com>
2023-10-26 16:14:40 -0400

    Merge commit 'c362703aeadc4a9ea7dafa95958a817f6dc98417'

--- src/Components/ActuContent.svelte
+++ src/Components/ActuContent.svelte
@@ -1,9 +1,60 @@
 <script lang="ts">
 	import type { Article } from '$comp/ActuCtr.svelte';
 	import { selected_cats_store, selected_pubs_store } from '$lib/actustores';
+	import { afterUpdate, beforeUpdate } from 'svelte';
 	export let articles: Article[];
 	// export let selected_pubnames: string[];
 	export let collapse_summary: boolean;
+
+	/* * before update, get id of top elt in pagecontent viewport*/
+	let topElementId: string = '';
+	//
+	beforeUpdate(() => {
+		// console.log('before update called');
+
+		const cardElts = document.getElementsByClassName('card');
+		const parent = document.getElementById('pagecontent');
+		if (!parent) return;
+		const top = parent.scrollTop;
+		const height = parent.offsetHeight;
+		if (topElementId && !collapse_summary) {
+			// console.log('b4: expdg, so scroll to topElementId\n', topElementId);
+			let el = document.getElementById(topElementId) as HTMLDivElement;
+			if (el) {
+				el.scrollIntoView(true);
+			} else {
+				el = cardElts[0] as HTMLDivElement;
+			}
+		}
+		if (collapse_summary) {
+			for (let i = cardElts.length - 1; i >= 0; i--) {
+				let el = cardElts.item(i) as HTMLDivElement;
+				let y = el.offsetTop;
+				// console.log('y', y);
+				// check if el is visible in container
+				if (top && height && y >= top && y <= top + height) {
+					topElementId = el.id;
+					// console.log('setting topElementId', topElementId);
+					// break out after first visible el, which will be top one
+					break;
+				}
+			}
+		}
+	});
+
+	/* * After update, find top elt and scroll to it*/
+	afterUpdate(() => {
+		// console.log('after update topElId', topElementId);
+
+		if (topElementId) {
+			// console.log('aft: scroll to topElementId\n', topElementId);
+			const el = document.getElementById(topElementId) as HTMLDivElement;
+			if (el) {
+				el.scrollIntoView(true);
+			}
+		}
+	});
+
 	function isCatShowing(cat: string): boolean {
 		const selected_cats = $selected_cats_store;
 		let retval: boolean;
@@ -24,7 +75,7 @@
 		{#if isCatShowing(article.cat ?? 'uncategorized') && $selected_pubs_store.includes(article.pubname)}
 			<!-- content here -->
 
-			<div class="card">
+			<div id={article.hash} class="card">
 				<div class="cardhdr" class:nosumm={collapse_summary}>
 					<!-- <span class="pubdate">[{article.pubdate}: {article.pubname}-{article.hash}]</span> -->
 					<div class="pubdate">[{article.pubdate}: {article.pubname}]</div>
