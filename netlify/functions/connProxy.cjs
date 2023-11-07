8a0d5ebbc6a9354b2a23397694bbb8b78451869b
Art Goldhammer <art.goldhammer@gmail.com>
2023-11-07 10:08:20 -0500

    removed useless radar check

--- netlify/functions/connProxy.cjs
+++ netlify/functions/connProxy.cjs
@@ -1,18 +1,9 @@
 exports.handler = async event => {
-  // * check backend alive
-  const radar_uri = process.env.RADAR_URI;
-	const radar_resp = await fetch(radar_uri);
-  if (radar_resp.status !== 204) {
-    console.error('Backend not responding');
-  }
-
-  // console.log ('event qsp', event.queryStringParameters);
+  
   const tf = event.queryStringParameters.timeframe || '0';
   const tw = event.queryStringParameters.timewindow || '2';
   const tx = event.queryStringParameters.txtquery;
-  // console.log ('tf', tf);
-  // console.log ('tw', tw);
-  // console.log ('tx', tx);
+  
 
   const proxy_uri = process.env.PROXY_URI;
   let uri;
